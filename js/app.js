12a023cfbe327ea64a5a63daab47307ad9a56083
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2024-12-27 01:19:55 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (gameClicado.classList.contains('dashboard__item__img--rented'), 
+       (gameClicado.classList.contains('dashboard__item__button--return')), 
+       (gameClicado.classList.contains('dashboard__item__img')), 
+       (gameClicado.classList.contains('dashboard__item__button'))) {
+        gameClicado.classList.remove('dashboard__item__img--rented');
+        gameClicado.classList.remove('dashboard__item__button--return');
+        gameClicado.classList.add('dashboard__item__img');
+        gameClicado.classList.add('dashboard__item__button');
+        gameClicado.innerText = 'Alugar';
+    } else { 
+        gameClicado.classList.remove('dashboard__item__img');
+        gameClicado.classList.remove('dashboard__item__button');
+        gameClicado.classList.add('dashboard__item__img--rented');
+        gameClicado.classList.add('dashboard__item__button--return');
+        gameClicado.innerText = "Devolver";
+    }
+}
