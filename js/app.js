5105633aa211bc89f8dbb69717bdfa8a0901092a
Joao Pedro Gonzatti <joaopedro.gonzatti@hotmail.com>
2024-12-27 15:00:45 -0300

    Ajustado a lógica condicional if

--- js/app.js
+++ js/app.js
@@ -2,22 +2,15 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
 
-    if (gameClicado.classList.contains('dashboard__item__img--rented'), 
-       (gameClicado.classList.contains('dashboard__item__button--return')), 
-       (gameClicado.classList.contains('dashboard__item__img')), 
-       (gameClicado.classList.contains('dashboard__item__button'))) {
-        gameClicado.classList.remove('dashboard__item__img--rented');
-        gameClicado.classList.remove('dashboard__item__button--return');
-        gameClicado.classList.add('dashboard__item__img');
-        gameClicado.classList.add('dashboard__item__button');
-        gameClicado.innerText = 'Alugar';
-    } else { 
-        gameClicado.classList.remove('dashboard__item__img');
-        gameClicado.classList.remove('dashboard__item__button');
-        gameClicado.classList.add('dashboard__item__img--rented');
-        gameClicado.classList.add('dashboard__item__button--return');
-        gameClicado.innerText = "Devolver";
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented')
+        botao.classList.remove('dashboard__item__button--return')
+        botao.textContent = 'Alugar'
+
+    }else {
+        imagem.classList.add('dashboard__item__img--rented')
+        botao.classList.add('dashboard__item__button--return')
+        botao.textContent = 'Devolver'
     }
 }
